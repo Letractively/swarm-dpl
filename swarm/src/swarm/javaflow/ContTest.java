6c08fa2b7848163f4344fcc53822e34a3fcb23f1
ian.clarke <ian.clarke@d8135746-d7fb-11dd-90d0-c9c6575c7768>
2009-01-18 17:16:33 +0000

    initial commit

---
+++ swarm/src/swarm/javaflow/ContTest.java
@@ -0,0 +1,16 @@
+package swarm.javaflow;
+
+import org.apache.commons.javaflow.Continuation;
+
+public class ContTest implements Runnable {
+	  public void run() {
+		System.out.println("started!");
+		for (int i = 0; i < 10; i++)
+			echo(i);
+	}
+
+	private void echo(int x) {
+		System.out.println(x);
+		Continuation.suspend();
+	}
+}
